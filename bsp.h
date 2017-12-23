87864bb2b1552461861528fa683fe586e258764b
qrshi <346982857@qq.com>
2017-12-23 16:15:20 +0800

    Create bsp.h

---
+++ bsp.h
@@ -0,0 +1,27 @@
+#ifndef __BSP_H__
+#define	__BSP_H__
+
+//struct _IntArray;
+typedef struct _IntArray IntArray;
+
+
+extern unsigned char gdata;//变量声明
+static unsigned char sdata=0;//变量定义
+
+
+
+unsigned char basp(void);//函数声明，默认前面extern 用于外部访问时推荐添加extern
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
